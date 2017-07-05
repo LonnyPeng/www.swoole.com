652c8081ed52b58529f31bf2002364fea25d6f73
lonny <lonny.p@eyebuydirect.com>
2017-07-05 15:43:30 +0800

    auto

---
+++ ebd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /c/Users/lonny.EYEBUYDIRECT/Desktop/swoole
+git add .
+git commit -m 'auto'
+
+git checkout master
+
+git pull origin master
+git push origin master
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
-phpinfo();
+$http = new swoole_http_server("0.0.0.0", 9501);
+
+print_r($http);die;
